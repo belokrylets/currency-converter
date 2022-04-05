26c9f4dfc0e1b83eaa4665f2fc63dc31a0ae43c2
Sergey Belokrylets <s.belokrylets@gmail.com>
2022-04-05 14:34:57 +0300

    рефакторинг Navbars

--- src/components/Navbars.tsx
+++ src/components/Navbars.tsx
@@ -4,15 +4,15 @@ import { Container, Nav, Navbar } from 'react-bootstrap';
 const Navbars: React.FC = () => {
     return (
         <div>
-              <Navbar bg="light" variant="light">
-    <Container>
-    <Navbar.Brand>Конвертер валют</Navbar.Brand>
-    <Nav className="me-auto">
-      <Nav.Link href="/">Конвертер</Nav.Link>
-      <Nav.Link href="/CurrencyRates">Курсы</Nav.Link>
-    </Nav>
-    </Container>
-  </Navbar>
+          <Navbar bg="light" variant="light">
+            <Container>
+              <Navbar.Brand>Конвертер валют</Navbar.Brand>
+              <Nav className="me-auto">
+                <Nav.Link href="/">Конвертер</Nav.Link>
+                <Nav.Link href="/CurrencyRates">Курсы</Nav.Link>
+              </Nav>
+            </Container>
+          </Navbar>
         </div>
     );
 }
